951a65f3074af72f46813bf74862f9a17ef7c424
Matthew Franglen <matthew@franglen.org>
2014-08-20 23:22:40 +0100

    Reapply styles
    Allowing styles to flow into the shadow dom no longer works.

--- example/web/my-tweets.dart
+++ example/web/my-tweets.dart
@@ -2,9 +2,6 @@ import 'package:polymer/polymer.dart';
 
 @CustomTag('my-tweets')
 class MyTweetsElement extends PolymerElement {
-  // This lets the Bootstrap CSS "bleed through" into the Shadow DOM
-  // of this element.
-  bool get applyAuthorStyles => true;
 
   @observable List<Tweet> tweets = toObservable([]);
 
