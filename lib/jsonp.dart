1a7f4b912899640cc9fdf07ba82d61214534d437
Matthew Franglen <matthew@franglen.org>
2016-02-11 20:35:08 +0000

    Remove type

--- lib/jsonp.dart
+++ lib/jsonp.dart
@@ -5,16 +5,19 @@ import 'src/jsonp_impl.dart' as impl;
 import 'src/external.dart';
 import 'src/ext_web.dart';
 
-Future fetch({String uri: null, String uriGenerator(String callback): null, Type type: null}) =>
+Future fetch({String uri: null, String uriGenerator(String callback): null}) =>
   impl.fetch(
       const External(const JavascriptImpl(), const HtmlImpl()),
-      uri: uri, uriGenerator: uriGenerator, type: type
+      uri: uri,
+      uriGenerator: uriGenerator
     );
 
-Stream fetchMany(String stream, {String uri: null, String uriGenerator(String callback): null, Type type: null}) =>
+Stream fetchMany(String stream, {String uri: null, String uriGenerator(String callback): null}) =>
     impl.fetchMany(
         const External(const JavascriptImpl(), const HtmlImpl()),
-        stream, uri: uri, uriGenerator: uriGenerator, type: type
+        stream,
+        uri: uri,
+        uriGenerator: uriGenerator
       );
 
 void disposeMany(String stream) => impl.disposeMany(stream);
