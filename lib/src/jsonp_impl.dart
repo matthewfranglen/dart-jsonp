4989fff145890cfcabf9cfd0d37276cd6073f294
Matthew Franglen <matthew@franglen.org>
2016-02-11 21:18:40 +0000

    Removing the external code

--- lib/src/jsonp_impl.dart
+++ lib/src/jsonp_impl.dart
@@ -2,13 +2,12 @@ library jsonp.impl;
 
 import 'dart:async';
 import 'handlers.dart';
-import 'external.dart';
 
-Future fetch(External external, {String uri: null, String uriGenerator(String callback): null}) {
+Future fetch({String uri: null, String uriGenerator(String callback): null}) {
   try {
-    final Once once = new Once(external);
+    final Once once = new Once();
 
-    once.request((String callback) => _generate_url(uri, uriGenerator, callback));
+    once.request((String callback) => _generateUrl(uri, uriGenerator, callback));
     return once.future();
   }
   catch (e) {
@@ -16,12 +15,12 @@ Future fetch(External external, {String uri: null, String uriGenerator(String ca
   }
 }
 
-Stream fetchMany(External external, String stream, {String uri: null, String uriGenerator(String callback): null}) {
-  final Many many = new Many(external, stream);
+Stream fetchMany(String stream, {String uri: null, String uriGenerator(String callback): null}) {
+  final Many many = new Many(stream);
 
   if (uri != null || uriGenerator != null) {
     try {
-      many.request((String callback) => _generate_url(uri, uriGenerator, callback));
+      many.request((String callback) => _generateUrl(uri, uriGenerator, callback));
     }
     catch (e) {
       many.error(e);
@@ -34,21 +33,17 @@ void disposeMany(String stream) {
   Many.dispose(stream);
 }
 
-/**
- * Transforms the uri, uriGenerator and callback into the callable url.
- */
-String _generate_url(String uri, String uriGenerator(String callback), String callback) {
+// Transforms the uri, uriGenerator and callback into the callable url.
+String _generateUrl(String uri, String uriGenerator(String callback), String callback) {
   if ( uri == null && uriGenerator == null ) {
     throw new ArgumentError("Missing Parameter: uri or uriGenerator required");
   }
 
-  return uri != null ? _add_callback_to_uri(uri, callback) : uriGenerator(callback);
+  return uri != null ? _addCallbackToUri(uri, callback) : uriGenerator(callback);
 }
 
-/**
- * Replaces any of the query values that are '?' with the callback name.
- */
-String _add_callback_to_uri(String uri, String callback) {
+// Replaces any of the query values that are '?' with the callback name.
+String _addCallbackToUri(String uri, String callback) {
   Uri parsed, updated;
   Map<String, String> query;
   int count = 0;
